ffab985808636460386e1d0bf70fc021264ee8a6
Fantoom <serge.vardanyan00@mail.ru>
2020-11-26 14:26:44 +0400

    Update App.xaml.cs

--- WallEngine/App.xaml.cs
+++ WallEngine/App.xaml.cs
@@ -21,10 +21,11 @@ namespace WallEngine
 
 			for (int index = 1; index < args.Length; index++)
 			{
-				if (args[index].Contains("--")) { 
-				string arg = args[index].Replace("--", "");
-				arguments.Add(arg, args[index + 1]);
-				Console.WriteLine(arg + "   "  + args[index + 1]);
+				if (args[index].Contains("--")) 
+				{ 
+					string arg = args[index].Replace("--", "");
+					arguments.Add(arg, args[index + 1]);
+					Console.WriteLine(arg + "   "  + args[index + 1]);
 				}
 				if (args[index].Contains("-"))
 				{
